8ecddcd795809e3405a3fe536e09da8ccd533f95
USIGLOBAL\daniel_tsai <daniel_tsai@usiglobal.com>
2024-06-19 15:18:14 +0800

    update ZJ new line and log format

--- download.py
+++ download.py
@@ -1,6 +1,5 @@
 import argparse
 import requests
-import re
 import logging
 import json
 from tqdm import tqdm
@@ -14,18 +13,21 @@ class Logger:
     def __init__(self, debug_file='debug.log', name='main', level='DEBUG', format='[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S') -> None:
         self.logger = logging.getLogger(name=name)
         self.logger.setLevel(self.getLevel(level))
+        self.logger.propagate = False
+        
+        if self.logger.hasHandlers():
+            self.logger.handlers.clear()
+            
+        self.file_logger = logging.FileHandler(debug_file, mode='a')
+        self.file_logger.setLevel(self.getLevel(level))
+        self.file_logger.setFormatter(logging.Formatter(format))
 
-        if not self.logger.handlers:
-            self.file_logger = logging.FileHandler(debug_file, mode='a')
-            self.file_logger.setLevel(self.getLevel(level))
-            self.file_logger.setFormatter(logging.Formatter(format))
+        self.stream_logger = logging.StreamHandler()
+        self.stream_logger.setLevel(self.getLevel(level))
+        self.stream_logger.setFormatter(logging.Formatter(format))
 
-            self.stream_logger = logging.StreamHandler()
-            self.stream_logger.setLevel(self.getLevel(level))
-            self.stream_logger.setFormatter(logging.Formatter(format))
-
-            self.logger.addHandler(self.file_logger)
-            self.logger.addHandler(self.stream_logger)
+        self.logger.addHandler(self.file_logger)
+        self.logger.addHandler(self.stream_logger)
 
     def getLevel(self, level_string):
         if level_string == 'DEBUG':
@@ -42,24 +44,24 @@ class Logger:
             return logging.CRITICAL
 
     @classmethod
-    def debug(cls, message):
-        cls().logger.debug(message)
+    def debug(cls, messages):
+        cls().logger.debug(messages)
 
     @classmethod
-    def info(cls, message):
-        cls().logger.info(message)
+    def info(cls, messages):
+        cls().logger.info(messages)
 
     @classmethod
-    def warn(cls, message):
-        cls().logger.warn(message)
+    def warn(cls, messages):
+        cls().logger.warn(messages)
 
     @classmethod
-    def error(cls, message):
-        cls().logger.error(message)
+    def error(cls, messages):
+        cls().logger.error(messages, exc_info=True)
 
     @classmethod
-    def critical(cls, message):
-        cls().logger.critical(message)
+    def critical(cls, messages):
+        cls().logger.critical(messages)
         
 
 class ImageDownloader:
@@ -97,8 +99,8 @@ class ImageDownloader:
     def _get_database_settings(self, configs, site):
         return configs[site]['database']
     
-    def _get_image_list(self, ssh_tunnel, database, site, start_date, end_date, **kwargs):
-        Logger.info(f'Getting image list from {site} site')
+    def _get_image_list(self, ssh_tunnel, database, image_pools, site, start_date, end_date, **kwargs):
+        Logger.info(f'Getting image list from {site} site {kwargs["line_id"]} line')
         with create_session(ssh_tunnel, database, site) as session:
             query = session.query(AmrRawData).filter(AmrRawData.create_time.between(start_date, end_date))
             if kwargs['group_type_list']:
@@ -113,23 +115,30 @@ class ImageDownloader:
             for key, value in kwargs.items():
                 if value and key in AmrRawData.__table__.columns.keys():
                     query = query.filter(getattr(AmrRawData, key) == value)
+            
+            if kwargs['limit']:
+                query = query.limit(kwargs['limit'])
 
-                
             data = query.all()
             
-            Logger.info(f'Found {len(data)} images')
+            Logger.info(f'Found {len(data)} images from {site} site {kwargs["line_id"]} line')
             
         images_list = []
         
         for obj in data:
-            images_list.append(obj.image_path)
-            
+            if site == 'NK':
+                images_list.append(f'images/{image_pools[obj.line_id]["line_ip"]}/{obj.image_path}')
+            elif site == 'ZJ':
+                images_list.append(f'{obj.line_id}/{obj.image_path}')
+            else:
+                images_list.append(obj.image_path)
+
         return images_list
 
-    def download(self, site, image_list, download_name, url):
-        Logger.info(f'Downloading {len(image_list)} images from {site} site')
+    def download(self, site, line_id, image_list, download_name, url):
+        Logger.info(f'Downloading {len(image_list)} images from {site} site {line_id} line')
         Logger.info(f'Download URL: {url}')
-        
+
         download_proxies = {'http': url}
         response = requests.post(url, proxies=download_proxies, json={
             "paths": image_list
@@ -162,29 +171,32 @@ class ImageDownloader:
         if self.kwargs['line_id']:
             line_id = self.kwargs['line_id']
             url = self._get_url(ssh_tunnel, image_pools[line_id])
-            images_list = self._get_image_list(ssh_tunnel, database, **self.kwargs)
-            self.download(self.kwargs['site'], images_list, f'{self.kwargs["download_name"]}_{line_id}', url)
-            self.server.stop()
+            images_list = self._get_image_list(ssh_tunnel, database, image_pools, **self.kwargs)
+            self.download(self.kwargs['site'], line_id, images_list, f'{self.kwargs["download_name"]}_{line_id}', url)
+            if self.server:
+                self.server.stop()
         elif self.kwargs['line_id_list'] == []:
             for line_id in image_pools:
                 self.kwargs['line_id']  = line_id
                 url = self._get_url(ssh_tunnel, image_pools[line_id])
-                images_list = self._get_image_list(ssh_tunnel, database, **self.kwargs)
-                self.download(self.kwargs['site'], images_list, f'{self.kwargs["download_name"]}_{line_id}', url)
-                self.server.stop()
+                images_list = self._get_image_list(ssh_tunnel, database, image_pools, **self.kwargs)
+                self.download(self.kwargs['site'], line_id, images_list, f'{self.kwargs["download_name"]}_{line_id}', url)
+                if self.server:
+                    self.server.stop()
         else:
             for line_id in self.kwargs['line_id_list']:
                 self.kwargs['line_id'] = line_id
                 url = self._get_url(ssh_tunnel, image_pools[line_id])
-                images_list = self._get_image_list(ssh_tunnel, database, **self.kwargs)
-                self.download(self.kwargs['site'], images_list, f'{self.kwargs["download_name"]}_{line_id}', url)
-                self.server.stop()
+                images_list = self._get_image_list(ssh_tunnel, database, image_pools, **self.kwargs)
+                self.download(self.kwargs['site'], line_id, images_list, f'{self.kwargs["download_name"]}_{line_id}', url)
+                if self.server:
+                    self.server.stop()
 
 def parse_opt():
     parser = argparse.ArgumentParser(description='Download images from image pool')
     parser.add_argument('--product-name', help='Product name')
-    parser.add_argument('--site', help='Site name e.g. JQ, ZJ, NK, HZ, etc.')
-    parser.add_argument('--line-id', help='Line ID')
+    parser.add_argument('--site', help='Site name e.g. JQ, ZJ, NK, HZ, etc.', required=True)
+    parser.add_argument('--line-id', help='Line ID', default='')
     parser.add_argument('--station-id', help='Station ID')
     parser.add_argument('--factory', help='Factory name')
     parser.add_argument('--aoi-id', help='AOI ID')
@@ -212,9 +224,11 @@ def parse_opt():
     parser.add_argument('--comp-type', help='Component type')
     parser.add_argument('--group-type', help='Group type')
     parser.add_argument('--is-covered', action='store_true')
-    parser.add_argument('--start-date', help='Start date, e.g. 2021-01-01')
-    parser.add_argument('--end-date', default=datetime.now().strftime('%Y-%m-%d'), help='End date, e.g. 2021-01-01')
+    parser.add_argument('--limit', type=int, help='Limit')
+    parser.add_argument('--start-date', help='Start date, e.g. 2021-01-01', required=True)
+    parser.add_argument('--end-date', default=datetime.now().strftime('%Y-%m-%d'), help='End date, e.g. 2021-01-01', required=True)
     parser.add_argument('--group-type-list', nargs='+', help='Group type list')
+    parser.add_argument('--part-number-list', nargs='+', help='Part Number List')
     parser.add_argument('--component-preffix-name', help='Component preffix name, e.g. L2002, FL6602 etc.')
     parser.add_argument('--line-id-list', nargs='+', help='Line ID list', default=[])
     parser.add_argument('--download-name', help='Download name', default='images')
